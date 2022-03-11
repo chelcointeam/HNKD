b041c281ffe202e803c32f4979f99d00eee19cfd
chelcointeam <97553734+chelcointeam@users.noreply.github.com>
2022-03-11 18:09:49 +0200

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,9 @@
+class Block {
+    constructor(index, previousHash, timestamp, data, hash) {
+        this.index = index;
+        this.previousHash = previousHash.toString();
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = hash.toString();
+    }
+}
